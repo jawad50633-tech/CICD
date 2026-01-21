2b34196c63b52c2c126f0e621e70ef441b4d0605
jawad50633-tech <jawad50633@gmail.com>
2026-01-21 22:44:55 +0500

    fixed

--- index.php
+++ index.php
@@ -3,45 +3,32 @@
 <html>
 <head>
     <title>Google Login</title>
+
+    <!-- REQUIRED -->
     <script src="https://accounts.google.com/gsi/client" async defer></script>
 </head>
 <body>
 
 <h2>Login with Google</h2>
 
+<!-- REQUIRED ROOT -->
 <div id="g_id_onload"
      data-client_id="<?= GOOGLE_CLIENT_ID ?>"
      data-callback="handleCredentialResponse">
 </div>
 
-<div class="g_id_signin"></div>
+<!-- BUTTON -->
+<div class="g_id_signin"
+     data-type="standard"
+     data-size="large"
+     data-theme="outline"
+     data-text="signin_with"
+     data-shape="rectangular">
+</div>
 
 <script>
 function handleCredentialResponse(response) {
-
-    fetch("google_callback.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            credential: response.credential
-        })
-    })
-    .then(res => res.json())
-    .then(data => {
-        console.log(data); // 👈 DEBUG
-
-        if (data.status === "success") {
-            window.location.replace("dashboard.php");
-        } else {
-            alert("Login failed");
-        }
-    })
-    .catch(err => {
-        console.error(err);
-        alert("Server error");
-    });
+    console.log("Token received"); // DEBUG
 }
 </script>
 
