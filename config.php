b247f4aedcab61d674f139051c401fadf12e9578
jawad50633-tech <jawad50633@gmail.com>
2026-01-23 11:05:59 +0500

    fixed isuue

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 session_start();
 
 define('GOOGLE_CLIENT_ID', '923173727502-ai0dtuino6cabrs3p9umm7s2pbl8hdbd.apps.googleusercontent.com');
-define('GOOGLE_REDIRECT_URI', 'http://devops.ct.ws/dashboard.php');
+define('GOOGLE_REDIRECT_URI', 'http://devops.ct.ws/google_callback.php');
